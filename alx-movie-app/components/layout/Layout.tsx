f502271d3aff2e4bd0abc2c39c887f89d51bdf72
BetNoelM <noelm@betsoftware.com>
2025-09-20 13:20:28 +0200

    add files

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-
-const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-return (
-<div>
-{children}
-</div>
-);
-};
-
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
+}
 
 export default Layout;
